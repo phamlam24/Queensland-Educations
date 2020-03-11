7d4caf5f7b944a2a8e06129685c4f19bc17abbb5
phamlam24 <ptlam61004@gmail.com>
2020-03-12 02:06:13 +0700

    Add forgetting password

--- kiddos/kiddos/js/loginsignup.js
+++ kiddos/kiddos/js/loginsignup.js
@@ -33,96 +33,20 @@ $(window).load(function () {
      $('.popupCloseButton').click(function(){
          $('.hover_bkgr_fricc_signup').hide();
      });
-     $('.txt1').click(function(){
-        $('.hover_bkgr_fricc_login').hide();
-        setTimeout($('.hover_bkgr_fricc_signup').fadeIn(),500);
+     $(".trigger_popup_fricc_forget").click(function(){
+        $('.hover_bkgr_fricc_forget').fadeIn();
+        just_opened = true;
+     });
+    //  $('.hover_bkgr_fricc_signup').click(function(){
+    //      $('.hover_bkgr_fricc_signup').hide();
+    //  });
+     $('.popupCloseButton').click(function(){
+         $('.hover_bkgr_fricc_forget').hide();
+     });
+     $('.send_email').click(function(){
+         alert("An email has been sent to your address. Follow the instructions in the email to reset your password.");
+         $('.hover_bkgr_fricc_forget').fadeOut();
      })
-    //  $(document).click(function (e){
-
-    //     var container = $("#loginpopup");
-    
-    //     if (!container.is(e.target) && container.has(e.target).length === 0 && !just_opened){
-    
-    //         container.fadeOut();
-            
-    //     }
-    //     if(just_opened){
-    //         just_opened = false;
-    //     }
-    //     console.log(just_opened);
-
-    // });
-    // $(document).click(function (e){
-    
-    //     var container = $("#signuppopup");
-    
-    //     if (!container.is(e.target) && container.has(e.target).length === 0 && !just_opened){
-    
-    //         container.fadeOut();
-            
-    //     }
-    //     if(just_opened){
-    //         just_opened = false;
-    //     }
-    //     console.log(just_opened);
-    // });
 });
-function reloadCss()
-{
-    var links = document.getElementsByTagName("link");
-    for (var cl in links)
-    {
-        var link = links[cl];
-        if (link.rel === "stylesheet")
-            link.href += "";
-    }
-}
-function confirmSignup(){
-    var SignupSuccessful = true;
-    // CONVERT TO VARIABLES:
-    var firstname_tag = document.getElementById("firstname");
-    var lastname_tag = document.getElementById("lastname");
-    var mail_tag = document.getElementById("mail");
-    var username_tag = document.getElementById("username");
-    var password_tag = document.getElementById("passtmp1");
-    var conf_password_tag = document.getElementById("passtmp2");
-    var firstname = firstname_tag.innerHTML;
-    var lastname = lastname_tag.innerHTML;
-    var mail = mail_tag.innerHTML;
-    var username = username_tag.innerHTML;
-    var password = password_tag.innerHTML;
-    var conf_password = conf_password_tag.innerHTML;
-    firstname = firstname.trim();
-    lastname = lastname.trim();
-    mail = mail.trim();
-    username = username.trim();
-    // NAMECHECK:
-    if(firstname.length >= 20){
-        firstname_tag.setAttribute("data-validate","Name too long");
-    }
-    else if(firstname.length == 0){
-        firstname_tag.setAttribute("data-validate","Please enter your first name");
-    }
-    if(lastname.length >= 20){
-        lastname_tag.setAttribute("data-validate","Name too long");
-    }
-    else if(lastname.length == 0){
-        lastname_tag.setAttribute("data-validate","Please enter your last name");
-    }
-    // TODO: ADD EMAIL FUNCTION (check if email already taken)
-    // USERNAME:
-    if(username.length > 15){
-        username_tag.setAttribute("data-validate","Username should not contain more than 15 letters");
-    }
-    else if(username.length < 6){
-        username_tag.setAttribute("data-validate","Username should not contain less than 6 letters");
-    }
-    else{
-        for(var i = 0;i<username.length;++i){
-            var current_char = username.charCodeAt(0);
-            if(!((current_char>64 && current_char<91) || (current_char>47 && current_char<58) || (current_char>96 && current_char<123))){
-                username_tag.setAttribute("data-validate","Username should not contain symbols");
-            }
-        }
-    }
-}
+
+
